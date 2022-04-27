731af947333b0205e4f20e07d4c7a626e059afee
PetterKokkim <petter.wessel.kokkim@gmail.com>
2022-04-27 22:48:58 +0100

    Added wifi module and coresponding code

--- SRC/ESP32/sketch_apr26a/sketch_apr26a.ino
+++ SRC/ESP32/sketch_apr26a/sketch_apr26a.ino
@@ -1,35 +1,18 @@
-#define BLYNK_PRINT Serial
-#define ESP8266_BAUD 9600
-#define BLYNK_TEMPLATE_ID "TMPLw_qpjb4o"
-#define BLYNK_DEVICE_NAME "MMPFishrobot"
 #define nrOfSteppers 1
-#define encoderA 2
-#define encoderB 3
 
-#include <ESP8266_Lib.h>
-#include <BlynkSimpleShieldEsp8266.h>
-#include <SoftwareSerial.h>
+const int stepperDirectionPin[nrOfSteppers] = {26};
+const int stepperPulsePin[nrOfSteppers] = {27};
 
-const int stepperDirectionPin[nrOfSteppers] = {11};
-const int stepperPulsePin[nrOfSteppers] = {10};
 
 int motorSpeed = 500;
 String menuselect;
 
-//Blynk auth code
-char auth[] = "p86vJfZq6gdOe6wOPgWEcw93mAwO5-1A";
-
-//Wifi login information
-char ssid[] = "G5Zone"; //You can replace the wifi name to your wifi
-char pass[] = "";  //Type password of your wifi.
-
-SoftwareSerial EspSerial(2, 3); // RX, TX
-WidgetLCD lcd(V0);
-
-ESP8266 wifi(&EspSerial);
-
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(100);
+
+  
   for (int s = 0; s < nrOfSteppers; s++) {
     pinMode(stepperDirectionPin[s], OUTPUT);
     digitalWrite(stepperDirectionPin[s], LOW);
@@ -37,28 +20,17 @@ void setup() {
     digitalWrite(stepperPulsePin[s], LOW);
   }
 
-  Serial.begin(9600);
-  delay(10);
-
-  EspSerial.begin(ESP8266_BAUD);
-  delay(10);
-
-  Blynk.begin(auth, wifi, ssid, pass);
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Blynk.run();
-
-  /*
-    Serial.println("---Welcome to fishrobot fish tail tester---");
-    Serial.println(" 1 --- Start motor");
-    Serial.println(" 2 --- Set motor speed");
-    Serial.println(" 3 --- Move one roation");
-    Serial.println(" 4 --- Move manually");
-    Serial.println(" 0 --- Menu");
-    for (;;) {
+  Serial.println("---Welcome to fishrobot fish tail tester---");
+  Serial.println(" 1 --- Start motor");
+  Serial.println(" 2 --- Set motor speed");
+  Serial.println(" 3 --- Move one roation");
+  Serial.println(" 4 --- Move manually");
+  Serial.println(" 0 --- Menu");
+  for (;;) {
     switch (Serial.read()) {
       case '1':
         for (int i = 0; i < 20; i++) {
@@ -80,40 +52,9 @@ void loop() {
       case'0': return;
       default: continue;
     }
-    }
-  */
+  }
 }
 
-BLYNK_WRITE(V0) {
-  int pinValue = param.asInt();
-  if (pinValue < 0) {
-    for (int i = 0; i < 20; i++) {
-      stepper(0, 100, 1, motorSpeed);
-      stepper(0, 200, 0, motorSpeed);
-      stepper(0, 100, 1, motorSpeed);
-    }
-    Serial.println("Stopped");
-  }
-}
-BLYNK_WRITE(V1) {
-  int pinValue = param.asInt();
-  if (pinValue < 0) {
-    stepper(0, 400, 1, motorSpeed);
-    Serial.println("Stopped");
-  }
-}
-BLYNK_WRITE(V2) {
-  int pinValue = param.asInt();
-  if (pinValue < 0) {
-    stepper(0, 1, 1, motorSpeed);
-  }
-}
-BLYNK_WRITE(V3) {
-  int pinValue = param.asInt();
-  if (pinValue < 0) {
-    stepper(0, 1, 0, motorSpeed);
-  }
-}
 
 void stepper(int motor, int steps, boolean stepDirection, int Speed) {
   int currentDirection = digitalRead(stepperDirectionPin[motor]);
